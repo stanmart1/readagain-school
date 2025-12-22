78664f982c264c33c95ff92abf81fb0d60ad3bb0
Adelodun Peter <adelodunpeter69@gmail.com>
2025-12-22 23:29:34 +0100

    Fix api import path to lib/api in useChat hook

--- frontend/src/hooks/useChat.js
+++ frontend/src/hooks/useChat.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react';
-import api from '../services/api';
+import api from '../lib/api';
 import websocketService from '../services/websocketService';
 
 export const useChat = (roomId) => {

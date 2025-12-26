a851247cac8fabc5c321616e42d2df00b6297835
Adelodun Peter <adelodunpeter69@gmail.com>
2025-12-26 12:51:00 +0100

    Fix JSX closing tag structure in EReaderPreview

--- frontend/src/components/EReaderPreview.jsx
+++ frontend/src/components/EReaderPreview.jsx
@@ -244,6 +244,7 @@ export default function EReaderPreview() {
                   </div>
                 </div>
               </div>
+            </motion.div>
             </>
           )}
         </AnimatePresence>
